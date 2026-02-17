b9e0c15ac6cc35b42c93cd59da04137bc8cc08e0
Archit Kumar <architkumar314@gmail.com>
2026-02-17 09:28:44 +0530

    [Code]: Pyramid using character.

---
+++ Patterns/07_pyramid.js
@@ -0,0 +1,40 @@
+
+class Solution{
+
+    solutionFirst(size, char){
+
+        console.log(`\nSolution First:`)
+
+        for(let i = 0; i < size; i++){
+
+            for(let j = 0; j < (size*2 - 1); j++){
+                if( (j >= (size - i - 1)) && (j < size + i) ){
+                    process.stdout.write(char)
+                }
+                else{
+                    process.stdout.write(' ')
+                }
+            }
+
+            console.log();
+        }
+    }
+}
+
+let input = ''
+process.stdin.on('data', data => {
+    input += data
+})
+
+process.stdin.on('end', () => {
+
+    const inputData = input.split('\n')
+
+    const size = Number(inputData[0].trim())
+    console.log(`Row size of pyramid: ${size}`)
+
+    const character = inputData[1].trim()
+    console.log(`Character to print: ${character}`)
+
+    new Solution().solutionFirst(size, character)
+})
