9a928a4f90108f246f504e1c40f4a6690e270c0b
Archit Kumar <architkumar314@gmail.com>
2026-02-24 22:20:41 +0530

    [Code]: Print row increment right angle triangle with alpha

---
+++ Patterns/14_row_increment_right_angle_triangle_with_alpha.js
@@ -0,0 +1,34 @@
+
+class Solution {
+
+    solutionFirst(size){
+
+        console.log(`\nSolution First:`)
+
+        for(let i = 0; i < size; i++){
+            let num = 65
+
+            for(let j = 0; j <= i; j++){
+                process.stdout.write(String.fromCharCode(num + j) + ' ')
+            }
+
+            console.log()
+        }
+    }
+}
+
+let input = ''
+
+process.stdin.on('data', data => {
+    input += data
+})
+
+process.stdin.on('end', () => {
+
+    const inputData = input.split('\n')
+
+    const size = Number(inputData[0].trim())
+    console.log(`Row size of the triangle: ${size}`)
+
+    new Solution().solutionFirst(size)
+})
