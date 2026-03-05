afa47e2ad57567190ac2af0bac8e78fd296131e9
Archit Kumar <architkumar314@gmail.com>
2026-03-05 14:46:49 +0530

    [Code]: Print hollo diamond with characters

---
+++ Patterns/19_hollo_diamond_with_character.js
@@ -0,0 +1,48 @@
+
+class Solution{
+
+    solutionFirst(size, char){
+
+        console.log(`\nSolution First:`);
+        
+        for(let i = 0; i < size; i++){
+            for(let j = 0; j < size; j++){
+                if(i < Math.floor(size/2)){
+                    if( ( j >= Math.floor(size/2)-i) && ( j < Math.floor(size/2) + i)){
+                        process.stdout.write(' ')
+                        continue;
+                    }
+                }
+                else{
+                    if( ( j > i - Math.floor(size/2)) && ( j < (size-1-i) + Math.floor(size/2))){
+                        process.stdout.write(' ')
+                        continue;
+                    }
+                }
+
+                process.stdout.write('*')
+            }
+
+            console.log()
+        }
+    }
+}
+
+let input = ''
+
+process.stdin.on('data', data => {
+    input += data
+})
+
+process.stdin.on('end', () => {
+
+    const inputData = input.split('\n')
+    
+    const size = inputData[0].trim()
+    console.log(`Row size of an array: ${size}`);
+    
+    const character = inputData[1].trim()
+    console.log(`Character to print: ${character}`)
+    
+    new Solution().solutionFirst(size, character)
+})
